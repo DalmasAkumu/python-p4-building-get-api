1f97c8e6b82f914c1abc6ca15e962051f2878dcb
Dalmas <akumuodhiambo@gmail.com>
2023-12-07 16:05:07 +0300

    Improved

--- server/app.py
+++ server/app.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify
+from flask import Flask, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from sqlalchemy import desc
 
-from models import db, Bakery, BakedGood
-from sqlalchemy_serializer import SerializerMixin
+from models import db, User, Review, Game
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 migrate = Migrate(app, db)
 
@@ -17,47 +17,42 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return '<h1>Bakery GET API</h1>'
+    return "Index for Game/Review/User API"
 
-@app.route('/bakeries')
-def bakeries():
-    bakeries = Bakery.query.all()
+@app.route('/games')
+def games():
+
+    games = []
+    for game in Game.query.all():
+        game_dict = {
+            "title": game.title,
+            "genre": game.genre,
+            "platform": game.platform,
+            "price": game.price,
+        }
+        games.append(game_dict)
+
     response = make_response(
-        jsonify([bakery.to_dict() for bakery in bakeries]),
+        jsonify(games),
         200
     )
-    response.headers['Content-Type'] = 'application/json'
+    response.headers["Content-Type"] = "application/json"
+
     return response
 
-@app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-    bakery = Bakery.query.get(id)
-    response = make_response(
-        jsonify(bakery.to_dict(include=['baked_goods'])),
-        200
-    )
-    response.headers['Content-Type'] = 'application/json'
-    return response
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.filter_by(id=id).first()
 
-@app.route('/baked_goods/by_price')
-def baked_goods_by_price():
-    goods = BakedGood.query.order_by(desc(BakedGood.price)).all()
-    response = make_response(
-        jsonify([good.to_dict() for good in goods]),
-        200
-    )
-    response.headers['Content-Type'] = 'application/json'
-    return response
+    game_dict = game.to_dict()
 
-@app.route('/baked_goods/most_expensive')
-def most_expensive_baked_good():
-    most_expensive_good = BakedGood.query.order_by(desc(BakedGood.price)).first()
     response = make_response(
-        jsonify(most_expensive_good.to_dict()),
+        jsonify(game_dict),
         200
     )
-    response.headers['Content-Type'] = 'application/json'
+    response.headers["Content-Type"] = "application/json"
+
     return response
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
--- server/seed.py
+++ server/seed.py
@@ -1,26 +1,110 @@
 #!/usr/bin/env python3
 
+from random import randint, choice as rc
+
+from faker import Faker
+
 from app import app
-from models import db, Bakery, BakedGood
+from models import db, Game, Review, User
+
+genres = [
+    "Platformer",
+    "Shooter",
+    "Fighting",
+    "Stealth",
+    "Survival",
+    "Rhythm",
+    "Survival Horror",
+    "Metroidvania",
+    "Text-Based",
+    "Visual Novel",
+    "Tile-Matching",
+    "Puzzle",
+    "Action RPG",
+    "MMORPG",
+    "Tactical RPG",
+    "JRPG",
+    "Life Simulator",
+    "Vehicle Simulator",
+    "Tower Defense",
+    "Turn-Based Strategy",
+    "Racing",
+    "Sports",
+    "Party",
+    "Trivia",
+    "Sandbox"
+]
+
+platforms = [
+    "NES",
+    "SNES",
+    "Nintendo 64",
+    "GameCube",
+    "Wii",
+    "Wii U",
+    "Nintendo Switch",
+    "GameBoy",
+    "GameBoy Advance",
+    "Nintendo DS",
+    "Nintendo 3DS",
+    "XBox",
+    "XBox 360",
+    "XBox One",
+    "XBox Series X/S",
+    "PlayStation",
+    "PlayStation 2",
+    "PlayStation 3",
+    "PlayStation 4",
+    "PlayStation 5",
+    "PSP",
+    "PS Vita",
+    "Genesis",
+    "DreamCast",
+    "PC",
+]
+
+fake = Faker()
 
 with app.app_context():
 
-    BakedGood.query.delete()
-    Bakery.query.delete()
+    Review.query.delete()
+    User.query.delete()
+    Game.query.delete()
 
-    bakeries = [
-        Bakery(name='Delightful donuts'),
-        Bakery(name='Incredible crullers')
-    ]
-    db.session.add_all(bakeries)
-    db.session.commit()
+    users = []
+    for i in range(100):
+        u = User(name=fake.name(),)
+        users.append(u)
 
-    baked_goods = [
-        BakedGood(name='Chocolate dipped donut', price=2.75, bakery=bakeries[0]),
-        BakedGood(name='Apple-spice filled donut', price=3.50, bakery=bakeries[0]),
-        BakedGood(name='Glazed honey cruller', price=3.25, bakery=bakeries[1]),
-        BakedGood(name='Chocolate cruller', price=3.40, bakery=bakeries[1])
-    ]
+    db.session.add_all(users)
 
-    db.session.add_all(baked_goods)
+    games = []
+    for i in range(100):
+        g = Game(
+            title=fake.sentence(),
+            genre=rc(genres),
+            platform=rc(platforms),
+            price=randint(5, 60),
+        )
+        games.append(g)
+
+    db.session.add_all(games)
+
+    reviews = []
+    for u in users:
+        for i in range(randint(1, 10)):
+            r = Review(
+                score=randint(0, 10),
+                comment=fake.sentence(),
+                user=u,
+                game=rc(games))
+            reviews.append(r)
+
+    db.session.add_all(reviews)
+
+    for g in games:
+        r = rc(reviews)
+        g.review = r
+        reviews.remove(r)
+
     db.session.commit()
